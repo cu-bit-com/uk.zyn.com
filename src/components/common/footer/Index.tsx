91cfd6d0466b5c727ec2489f719249eb162e69ca
Antonio Rafajec <antonio.rafajec@gmail.com>
2024-03-21 09:31:35 +0100

    fix mobile header and hamburger menu

--- src/components/common/footer/Index.tsx
+++ src/components/common/footer/Index.tsx
@@ -3,27 +3,30 @@ import styles from './Footer.module.css';
 
 function Footer() {
     return (
-        <div className={global.wrapper}>
-            <footer id={styles.footer}>
-                <div className={`${styles.links} ${styles.item}`}>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><button className={styles.link}>FAQs</button></li>
-                        <li className={styles.listItem}><button className={styles.link}>What is ZYN</button></li>
-                        <li className={styles.listItem}><button className={styles.link}>Find ZYN</button></li>
-                    </ul>
-                </div>
-                <div className={`${styles.social} ${styles.item}`}>
-                    <ul className={styles.list}>
-                        <li className={styles.listItem}><button className={styles.link}>Facebook</button></li>
-                        <li className={styles.listItem}><button className={styles.link}>Instagram</button></li>
-                        <li className={styles.listItem}><button className={styles.link}>YouTube</button></li>
-                    </ul>
-                </div>
-                <div className={`${styles.contact} ${styles.item}`}>
-                    <button className={styles.link}>Contact Us</button>
-                </div>
-            </footer>
-        </div>
+        <>
+            <hr className={styles.hr} />
+            <div className={global.wrapper}>
+                <footer id={styles.footer}>
+                    <div className={`${styles.links} ${styles.item}`}>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}><button className={styles.link}>FAQs</button></li>
+                            <li className={styles.listItem}><button className={styles.link}>What is ZYN</button></li>
+                            <li className={styles.listItem}><button className={styles.link}>Find ZYN</button></li>
+                        </ul>
+                    </div>
+                    <div className={`${styles.social} ${styles.item}`}>
+                        <ul className={styles.list}>
+                            <li className={styles.listItem}><button className={styles.link}>Facebook</button></li>
+                            <li className={styles.listItem}><button className={styles.link}>Instagram</button></li>
+                            <li className={styles.listItem}><button className={styles.link}>YouTube</button></li>
+                        </ul>
+                    </div>
+                    <div className={`${styles.contact} ${styles.item}`}>
+                        <button className={styles.link}>Contact Us</button>
+                    </div>
+                </footer>
+            </div>
+        </>
     );
 }
 
--- src/components/common/header/NavigationMobile.tsx
+++ src/components/common/header/NavigationMobile.tsx
@@ -1,15 +1,41 @@
 import { Link } from 'react-router-dom';
 import styles from './NavigationMobile.module.css';
+import { useEffect, useRef } from 'react';
 
-function NavigationMobile() {
-    return(
-        <div className={styles.hamburgerMenu}>
-            <input id={styles.menuToggle} type="checkbox" />
-            <label className={styles.menuBtn} htmlFor={styles.menuToggle}>
-            <span></span>
+    const NavigationMobile: React.FC = () => {
+        const menuToggleRef = useRef<HTMLInputElement>(null);
+        const menuBoxRef = useRef<HTMLUListElement>(null);
+        const menuBtnRef = useRef<HTMLLabelElement>(null);
+
+        useEffect(() => {
+            const handleClickOutside = (event: MouseEvent) => {
+            if (
+                menuToggleRef.current &&
+                menuBoxRef.current &&
+                menuBtnRef.current &&
+                !menuToggleRef.current.contains(event.target as Node) &&
+                !menuBoxRef.current.contains(event.target as Node) &&
+                !menuBtnRef.current.contains(event.target as Node)
+            ) {
+                menuToggleRef.current.checked = false;
+            }
+            };
+
+            document.addEventListener('click', handleClickOutside);
+
+            return () => {
+            document.removeEventListener('click', handleClickOutside);
+            };
+        }, []);
+        
+        return(
+            <div className={styles.hamburgerMenu}>
+            <input id={styles.menuToggle} type="checkbox" ref={menuToggleRef}/>
+            <label className={styles.menuBtn} htmlFor={styles.menuToggle} ref={menuBtnRef}>
+                <span></span>
             </label>
 
-            <ul className={styles.menuBox}>
+            <ul className={styles.menuBox} ref={menuBoxRef}>
                 <li><Link className={`${styles.menuItem} ${styles.link}`} to='/'>Home</Link></li>
                 <li><Link className={`${styles.menuItem} ${styles.link}`} to='/shop'>Shop</Link></li>
                 <li><Link className={`${styles.menuItem} ${styles.link}`} to='/where-to-buy'>Where to Buy</Link></li>
@@ -18,7 +44,7 @@ function NavigationMobile() {
                 <li><Link className={`${styles.menuItem} ${styles.link}`} to='/contact'>Contact</Link></li>
             </ul>
         </div>
-    );
+        );
 }
 
 export default NavigationMobile;
