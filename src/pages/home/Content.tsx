a2ff33ee56a814af8df291185a4019fd227c48a7
MadaSosta <ivanmadarcic@gmail.com>
2024-04-25 17:22:19 +0200

    Homepage content adding

--- src/pages/home/Content.tsx
+++ src/pages/home/Content.tsx
@@ -1,12 +1,36 @@
-import styles from './Content.module.css';
-import global from '../../components/Global.module.css';
+import styles from "./Content.module.css";
+import global from "../../components/Global.module.css";
+import image from "../../images/zyn-item.jpeg";
 
 function Content() {
-    return (
-        <div className={global.wrapper}>
-            <p className={styles.p}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam laborum beatae corrupti vel, et mollitia tempora accusamus pariatur distinctio in possimus sit, exercitationem fugit! Repellendus, laborum. Veniam deserunt, ad excepturi ipsam voluptate odit consequatur, beatae quas tenetur temporibus nihil neque? Esse unde dignissimos, optio praesentium assumenda vero illo molestias iusto!</p>
+  return (
+    <div className={global.wrapper}>
+      <div className={styles.discover_zyn}>
+        <div className={styles.discover_zyn_info}>
+          <h3>Discover ZYN Nicotine Pouches</h3>
+          <p className={styles.p}>
+            Discover a tobacco-free alternative to smiking filled with flavours
+            and nicotine strenghts designed to satisfy your taste.
+          </p>
+          <button className={styles.learn_more}>
+            {" "}
+            <a href="#">Learn More About ZYN</a>
+          </button>
         </div>
-    );
+        <div className={styles.discover_zyn_img}>
+          <img src={image} alt="ZYN Nicotine Pouches" />
+        </div>
+      </div>
+      <div className={styles.explore_zyn}>
+        <h3>Explore ZYN Products</h3>
+        <p className={styles.p}>
+          If you are nicotine user looking for a new alternative, we suggest
+          starting with a lower nicotine strenght, try our Mini (6mg) pouches.
+          For nicotine pouch users, discover our Slim range!
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Content;
--- src/pages/home/Images.tsx
+++ src/pages/home/Images.tsx
@@ -1,15 +1,29 @@
-import styles from './Images.module.css';
-import global from '../../components/Global.module.css';
-import image from '../../images/zyn-item.jpeg';
+import styles from "./Images.module.css";
+import global from "../../components/Global.module.css";
+import image_one from "../../images/products/4294.png";
+import image_two from "../../images/products/4295.png";
+import image_three from "../../images/products/4296.png";
+import image_four from "../../images/products/4297.png";
+import image_five from "../../images/products/4299.png";
+import image_six from "../../images/products/4298.png";
+import image_seven from "../../images/products/4302.png";
+import image_eight from "../../images/products/4300.png";
+import image_nine from "../../images/products/4301.png";
 
 function Images() {
-    return (
-        <div className={`${global.wrapper} ${styles.container}`}>
-                <img src={image} className={styles.item}></img>
-                <img src={image} className={styles.item}></img>
-                <img src={image} className={styles.item}></img>
-        </div>
-    );
+  return (
+    <div className={`${global.wrapper} ${styles.container}`}>
+      <img src={image_one} className={styles.item}></img>
+      <img src={image_two} className={styles.item}></img>
+      <img src={image_three} className={styles.item}></img>
+      <img src={image_four} className={styles.item}></img>
+      <img src={image_five} className={styles.item}></img>
+      <img src={image_six} className={styles.item}></img>
+      <img src={image_seven} className={styles.item}></img>
+      <img src={image_eight} className={styles.item}></img>
+      <img src={image_nine} className={styles.item}></img>
+    </div>
+  );
 }
 
 export default Images;
