eb570af5d8699441a32a9f5ae881fe4cd864bc6e
Rafo0x63 <antonio.rafajec@gmail.com>
2024-05-14 12:46:34 +0200

    minor changes

--- src/pages/discoverZyn/Carousel.tsx
+++ src/pages/discoverZyn/Carousel.tsx
@@ -17,7 +17,8 @@ const Carousel = () => {
         speed: 500,
         slidesToShow: 1,
         slidesToScroll: 1,
-        autoplay: false
+        autoplay: true,
+		autoplaySpeed: 5000
     };
 
     return (
