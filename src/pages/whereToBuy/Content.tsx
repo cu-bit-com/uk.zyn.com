743f6b1e7343e540a278c3f35740ee30c7ff73ae
Rafo0x63 <antonio.rafajec@gmail.com>
2024-05-14 12:41:31 +0200

    add WHSmith logo

--- src/pages/whereToBuy/Content.tsx
+++ src/pages/whereToBuy/Content.tsx
@@ -1,5 +1,7 @@
 import styles from './Content.module.css';
 import global from '../../components/Global.module.css';
+import image_one from '../../images/whereToBuy/15.png'
+import { Link } from 'react-router-dom';
 
 function Content() {
     return (
@@ -7,6 +9,10 @@ function Content() {
 			<h3>Find ZYN Nicotine Pouches in Store</h3>
 			<br />
             <p className={styles.p}>Find ZYN at Saintsbury’s, Morrisons BP, Waitrose & WHSmith’s Travel</p>
+
+			<div className={styles.logos}>
+				<Link to="https://www.whsmith.co.uk/"><img src={image_one} width="200px" alt="" /></Link>
+			</div>
         </div>
     );
 }
