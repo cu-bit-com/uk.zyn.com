58542deccbd41dbc46b5c045171170636e613095
Antonio Rafajec <antonio.rafajec@gmail.com>
2024-06-19 14:58:42 +0200

    small update

--- src/pages/faq/Title.tsx
+++ src/pages/faq/Title.tsx
@@ -3,7 +3,7 @@ import styles from './Title.module.css';
 function Title() {
     return (
         <div className={styles.wrapper}>
-            <h1 className={styles.h1}>FAQ</h1>
+            <h2 className={styles.title}>Welcome to ZYN nicotine pouches FAQs</h2>
         </div>
     );
 }
