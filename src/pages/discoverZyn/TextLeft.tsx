9f38eedc4661b0db1974ac6e2fe3731a19a5b0b9
Antonio Rafajec <antonio.rafajec@gmail.com>
2024-06-24 12:54:06 +0200

    add link to btn

--- src/pages/discoverZyn/TextLeft.tsx
+++ src/pages/discoverZyn/TextLeft.tsx
@@ -1,27 +1,29 @@
 import global from "../../components/Global.module.css";
 import styles from "./TextLeft.module.css";
 import placeholder from "../../images/discover/zyn_dj.jpg";
+import { Link } from "react-router-dom";
 
 function TextLeft() {
   return (
-	  <div className={`${global.wrapper}`}>
-		<br />
-		<br />
-	<h3>ZYN enhances every moment</h3>
-	<div className={styles.wrapper}>
-
-      <div className={styles.left}>
-        <p className={styles.colorWhite}>
-          Because we understand that your nicotine needs might change throughout
-          the day, we’ve crafted a range of products with varying nicotine
-          levels to satisfy your different nicotine moments and preferences
-        </p>
-		<button className={styles.buyBtn}>Find your ZYN</button>
+    <div className={`${global.wrapper}`}>
+      <br />
+      <br />
+      <h3>ZYN enhances every moment</h3>
+      <div className={styles.wrapper}>
+        <div className={styles.left}>
+          <p className={styles.colorWhite}>
+            Because we understand that your nicotine needs might change throughout
+            the day, we’ve crafted a range of products with varying nicotine
+            levels to satisfy your different nicotine moments and preferences
+          </p>
+          <Link to="/where-to-buy" onClick={() => window.scrollTo({ top: 0 })}>
+            <button className={styles.buyBtn}>Find your ZYN</button>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <img src={placeholder} className={styles.image} alt="" />
+        </div>
       </div>
-      <div className={styles.right}>
-        <img src={placeholder} className={styles.image} alt="" />
-      </div>
-	</div>
     </div>
   );
 }
